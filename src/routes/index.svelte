6e1480db6be464fb96506088a08630a9e587e218
hemanthttc <hemanth.narravula@ttc.com>
2021-04-28 12:27:39 +0100

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	// import Counter from '$lib/Counter/index.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="px-4 py-5 sm:px-6">
+	  <h3 class="text-lg leading-6 text-center font-medium text-gray-900">
+		Svelte & Tailwind CSS
+	  </h3>
+	  <p class="mt-1 max-w-2xl text-sm text-center text-gray-500">
+		Basic setup
+	  </p>
+	</div>
+	
+  
+</section>
+
+<style style lang="postcss">
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
